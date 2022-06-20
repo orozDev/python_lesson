0988bff3915017e70451c8afcbda65b94c5b250b
Orozbek <oroz.jenishbek.2003@gmail.com>
2022-06-20 13:21:18 +0600

    meassage

--- задании/while.py
+++ задании/while.py
@@ -1,1 +1,73 @@
-n = int(input)
+n = 0
+
+# for item in range(1,n+1,2): # cicle for
+#     print(item,'hello world')
+
+# бесконечный цикл
+# while True: 
+    # print('*')
+
+# chek = 1
+while n == 10:
+    print(n,'hello world')
+    # chek = chek + 1
+    n = n + 1
+      # cicle while 
+chek2 = 3
+while n != 0:
+    # 1
+    while n == 3:
+        print('bir')
+        n = n - 1
+    # 1
+    if n == 2:
+        #3
+        while n !=1:
+            print('hello!')
+            n = n - 1
+    if n == 1:
+        print('the end.')
+    n = n - 1
+    print(n)
+# задача 1:
+# input: 7
+# output: 
+# 70 
+# 60 
+# 50 
+# 40 
+# 30 
+# 20 
+# 10 
+# the end.
+
+name = 'Alex687'
+    #   0123456
+print(type(len(name))) 
+
+name2 = name[:4]
+print(name2)
+print(name[0],name[3],name[5])
+
+var_1 = 'Привет'
+var_2 = 'Мир'
+print(var_1 + ' ' + var_2)
+
+print('O_o ' * 9 )
+ 
+abc = 'abcdefgh'
+print('a' in abc)
+
+name = 'Tom'
+age = 17
+print(name +' '+ str(age))
+
+title = 'abcdh hhkjdhsjfhsdj dfs'
+print(title.count('j'))
+
+title = 'apamberdi pamidor the end.'
+print(title.startswith('apam')) #если строка начинаеться с этого элемента то True  а если не то  False
+print(title.endswith('the end')) # если строка заканчиваеться этим элементов то True а если не то  False
+print(title.find(' ')) # возвращает первый элемент индекса начало этого элемента
+print(title.index('pa')) # возвращает первый элемент индекса начало этого элемента
+
