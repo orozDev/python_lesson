6c05b7e2d976c88b75888cbd7a57d18e778c4275
Orozbek <oroz.jenishbek.2003@gmail.com>
2022-06-27 09:48:46 +0600

    27 06 2022

--- задании/change_place_of_variables.py
+++ задании/change_place_of_variables.py
@@ -1,3 +1,17 @@
-a = input()
-b = input()
+# ____FIST_PART_____
+# a = input()
+# b = input()
 
+# c = a
+# a = b
+# b = c
+
+# print(a, b)
+
+# ____SECOND_PART_____
+# a = int(input())
+# b = int(input())
+
+# a, b = b, a
+
+# print(a, b)
