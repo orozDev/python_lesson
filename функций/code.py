816be9d9d6c8a9cffec5be6746d8063ae8708a59
Orozbek <oroz.jenishbek.2003@gmail.com>
2022-06-27 09:07:15 +0600

    27 06 2022

--- функций/code.py
+++ функций/code.py
@@ -1,14 +1,35 @@
-a = int(input('1: '))
-b = int(input('2: '))
+# a = int(input('1: '))
+# b = int(input('2: '))
 
-def is_check(n, index):
-    if n % 2 == 0:
-        return str(index) + ' is even'
-    else:
-        return str(index) + ' is odd'
+# def is_check(n, index):
+#     if n % 2 == 0:
+#         return str(index) + ' is even'
+#     else:
+#         return str(index) + ' is odd'
 
-a = is_check(a, 'fist number')
+# a = is_check(a, 'fist number')
 
-print(a)
+# print(a)
+import math
+number = int(input(':'))
+lst = []
+def square(n):
+    p = n * 4
+    lst.append('perimetr')
+    lst.append(p)
+
+    pl = n ** 2
+    lst.append('ploshad')
+    lst.append(pl)
+
+    # di = n * math.sqrt(n)
+    di = ((2*n**2)**.5)
+
+    lst.append('diagonal')
+    lst.append(di)
+    print(lst)
+
+function = square(number)
+print(function)
 
 
