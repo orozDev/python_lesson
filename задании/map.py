badcc5f37f5679c3e6463a2387263a0c2bede158
Orozbek <oroz.jenishbek.2003@gmail.com>
2022-06-27 09:28:34 +0600

    27 06 2022

---
+++ задании/map.py
@@ -0,0 +1,11 @@
+# a, b = map(int, input().split())
+# print(a+b)
+
+array = input()
+array = list(map(int, array.split(' ')))
+
+a = [int('2'), int('3'), int('4'), int('5'), int('6')]
+map(int, a)
+
+print(array[0])
+print(type(array[0]))
