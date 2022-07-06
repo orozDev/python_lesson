58417007bfc3472c9288ab212d355bf7d678bb70
Orozbek <oroz.jenishbek.2003@gmail.com>
2022-07-06 10:03:37 +0600

    meevdfg

---
+++ OOП/quiz.py
@@ -0,0 +1,75 @@
+class Questions():
+    question = ''
+    answers = []
+    right_answer = 0
+
+    def __init__(self, question, answers, right_answer):
+        self.question = question
+        self.answers = answers
+        self.right_answer = right_answer
+
+    
+class Quiz():
+    title = ''
+    questions = []
+
+    def __init__(self, title, questions):
+        self.title = title
+        self.questions = questions
+
+fist_task = Questions(
+    '\nКто изабрель python ?',
+    ['Гвидо Ван Рассум', 'Кто-то', 'Не знаю'],
+    0,
+)
+
+second_task = Questions(
+    'Кто открыл компанию Google ?',
+    ['Сергей', 'Сергей брин и Лари Пейтж', 'Random name', 'Марк Цукеберг'],
+    1,    
+)
+
+def select_quiz(quizs):
+    for i in range(len(quizs)):
+        print(f'{i}: {quizs[i].title}')
+    n = int(input('\nВведите порядковый номер викторины: '))
+    if n - 1 > len(quizs) or n < 0:
+        print(f'\n{n}-го викторины не существует')
+    else:
+        quiz = quizs[n]
+        count = 0
+        print(f'\n    Виктарина "{quiz.title}" началось')
+        for question in quiz.questions:
+            print(f'{question.question}\n')
+            for answer in question.answers:
+                idx = question.answers.index(answer)
+                print(f'{idx}: {answer}')
+            temp = int(input('\nВведите вариант: '))
+            if temp == question.right_answer:
+                count += 1 
+        print(f'\nВаш балл составляет {count}')
+
+
+quiz = Quiz(
+    'Известные люди',
+    [fist_task, second_task],
+)
+
+quizs = [quiz]
+
+while True:
+    print(
+        '\nВыберитке пункт',
+        '\n1: Начать виктарину',
+        '\n2: Добавить виктарину',        
+    )
+
+    n = int(input('\nВведите порядковый номер пункта: '))
+
+    if n == 1:
+        select_quiz(quizs)
+    elif n == 2:
+        pass
+    else:
+        print('Досвидание')
+        break
